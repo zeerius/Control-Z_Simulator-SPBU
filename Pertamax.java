ce8bb331a76c64ff056d01949f094f24d6bc5655
Siti Mawaddah <sitimawaddahsakdan@gmail.com>
2020-12-22 19:49:32 +0700

    menambahkan atribut, constructor, dan method getter di class Pertamax

--- Pertamax.java
+++ Pertamax.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+
 public class Pertamax
 {
-    
+    private ArrayList<Pelanggan> pelangganPertamax;
+    private BahanBakar tangkiPertamax;
+
+    public Pertamax()
+    {
+        pelangganPertamax = new ArrayList<>();
+        tangkiPertamax = new BahanBakar();
+    }
+
+    public ArrayList<Pelanggan> getPelangganPertamax()
+    {
+        return this.pelangganPertamax;
+    }
+
+    public BahanBakar getTangkiPertamax()
+    {
+        return this.tangkiPertamax;
+    }
 }
