dc1905b3c05a3e7c751d39c827f2016430306797
Siti Mawaddah <sitimawaddahsakdan@gmail.com>
2020-12-23 01:01:38 +0700

    menambah method isiMinyak di class Solar

--- Solar.java
+++ Solar.java
@@ -41,7 +41,53 @@ public class Solar extends SPBU
     @Override
     public void isiMinyak()
     {
+        Scanner sc = new Scanner(System.in);
+        int biaya;
+        if(!cekIsiTangki()) {
+            tangkiSolar.tambah(tangkiSolar.getKapasitas() - tangkiSolar.getIsiTangki());
+        }
 
+        System.out.print("Berapa liter yang ingin Anda isi? ");
+        int liter = sc.nextInt();
+        biaya = liter * tangkiSolar.getHarga();
+        System.out.println("Biaya untuk " + liter + " liter solar adalah Rp" + biaya + ",00");
+
+        if (biaya > pelangganSolar.get(0).getUang()) {
+            int biayaKurang = biaya - pelangganSolar.get(0).getUang();
+            System.out.println("Maaf, uang yang Anda miliki kurang Rp" + biayaKurang + ",00");
+            System.out.print("Apakah Anda ingin menambah uang?(y/n) ");
+            String pilihan = sc.next();
+
+            if (pilihan.equalsIgnoreCase("y")) {
+                pelangganSolar.get(0).tambah(biayaKurang);
+            }
+            else if(cekUangPelanggan()){
+                int literCukup = pelangganSolar.get(0).getUang() / tangkiSolar.getHarga();
+                System.out.println("Dengan uang yang Anda miliki, Anda hanya dapat mengisi " + literCukup + " liter");
+                System.out.print("Apakah Anda tetap ingin mengisi?(y/n) ");
+                pilihan = sc.next();
+
+                if (pilihan.equalsIgnoreCase("y")) {
+                    liter = literCukup;
+                    biaya = liter * tangkiSolar.getHarga();
+                }
+                else {
+                    System.out.println("Selamat tinggal");
+                    hapusPelanggan();
+                    return;
+                }
+            }
+            else {
+                System.out.println("Anda tidak dapat melakukan pengisian bahan bakar");
+                hapusPelanggan();
+                return;
+            }
+        }
+        tangkiSolar.kurangi(liter);
+        pelangganSolar.get(0).kurangi(biaya);
+        pendapatan += biaya;
+        System.out.println("Bahan bakar berhasil diisi");
+        hapusPelanggan();
     }
 
     public boolean cekIsiTangki()
