36b0a7b118c4b27f97939dfb05182fc49de0888b
Siti Mawaddah <sitimawaddahsakdan@gmail.com>
2020-12-22 19:46:15 +0700

    menambahkan atribut, constructor, dan method getter di class Solar

--- Solar.java
+++ Solar.java
@@ -1,4 +1,23 @@
-public class Solar
-{
+import java.util.ArrayList;
 
+public class Solar extends SPBU
+{
+    private ArrayList<Pelanggan> pelangganSolar;
+    private BahanBakar tangkiSolar;
+
+    public Solar()
+    {
+        pelangganSolar = new ArrayList<>();
+        tangkiSolar = new BahanBakar();
+    }
+
+    public ArrayList<Pelanggan> getPelangganSolar()
+    {
+        return this.pelangganSolar;
+    }
+
+    public BahanBakar getTangkiSolar()
+    {
+        return this.tangkiSolar;
+    }
 }
