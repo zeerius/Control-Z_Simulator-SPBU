b3a403097d82ab1822d331b7f079abe5c0223dea
balqishumaira <66017795+balqishumaira@users.noreply.github.com>
2020-12-22 21:11:58 +0700

    membuat atribut,mengoverride method yang ada dalam interface TambahKurang
    meng-override method void tambah(int uang) dan method void kurangi(int uang) yang ada di dalam interface TambahKurang

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,3 +1,28 @@
-public class Pelanggan implements TambahKurang {
+/**
+ * Program yang berisi class Pelanggan
+ *
+ * @author Siti Balqis Humaira
+ */
 
+public class Pelanggan implements TambahKurang {
+	private int uang;
+	private String jenisKendaraan;
+  
+  /**
+	 * Method untuk menambahkan jumlah uang Pelanggan
+	 * @param uang
+	 */
+	@Override
+	public void tambah(int uang) {
+		this.uang += uang;
+	}
+
+	/**
+	 * Method untuk mengurangi jumlah uang Pelanggan
+	 * @param uang
+	 */
+	@Override
+	public void kurangi(int uang) {
+		this.uang -= uang;
+	}
 }
