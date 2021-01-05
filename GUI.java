12c41ae40dc3a60c2b27d6f876c118d23c647e52
Sulthan Zaidan Fauzi <66194310+zeerius@users.noreply.github.com>
2021-01-05 14:41:54 +0700

    menambahkan beberapa panel

--- GUI.java
+++ GUI.java
@@ -13,11 +13,68 @@ public class GUI extends javax.swing.JFrame
 
         panelFrame = new javax.swing.JPanel();
         panelUtama = new javax.swing.JPanel();
+        panelRegister = new javax.swing.JPanel();
+        panelIsiDataUser = new javax.swing.JPanel();
+        panelLogin = new javax.swing.JPanel();
+        panelMenu = new javax.swing.JPanel();
+        panelIsiMinyak = new javax.swing.JPanel();
+        panelCekSaldo = new javax.swing.JPanel();
+        panelTopUp = new javax.swing.JPanel();
+        panelProfil = new javax.swing.JPanel();
         
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Aplikasi goPERTA");
 
         panelFrame.setBackground(new java.awt.Color(196, 69, 105));
+        
+        
+        panelUtama.setLayout(new java.awt.CardLayout());
+
+        panelRegister.setBackground(new java.awt.Color(196, 69, 105));
+        panelRegister.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+        panelRegister.setFont(new java.awt.Font("Century Gothic", 0, 14));
+        
+        panelUtama.add(panelRegister, "card2");
+
+        panelIsiDataUser.setBackground(new java.awt.Color(196, 69, 105));
+        panelIsiDataUser.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+
+        panelUtama.add(panelIsiDataUser, "card9");
+
+        panelLogin.setBackground(new java.awt.Color(196, 69, 105));
+        panelLogin.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+        panelLogin.setToolTipText("");
+
+        panelUtama.add(panelLogin, "card3");
+
+        panelMenu.setBackground(new java.awt.Color(196, 69, 105));
+        panelMenu.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+
+        panelUtama.add(panelMenu, "card4");
+
+        panelIsiMinyak.setBackground(new java.awt.Color(196, 69, 105));
+        panelIsiMinyak.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+
+        panelUtama.add(panelIsiMinyak, "card5");
+
+        panelCekSaldo.setBackground(new java.awt.Color(196, 69, 105));
+        panelCekSaldo.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+
+        panelUtama.add(panelCekSaldo, "card6");
+
+        panelTopUp.setBackground(new java.awt.Color(196, 69, 105));
+        panelTopUp.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+
+        panelUtama.add(panelTopUp, "card7");
+
+        panelProfil.setBackground(new java.awt.Color(196, 69, 105));
+        panelProfil.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white));
+        panelProfil.setToolTipText("");
+
+        panelUtama.add(panelProfil, "card8");
+        
+        pack();
+        setLocationRelativeTo(null);
     }
 
     public static void main(String args[]) {
