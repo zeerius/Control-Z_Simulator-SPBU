98534e7d632d60083649f413b51f20a38087a263
Sulthan Zaidan Fauzi <66194310+zeerius@users.noreply.github.com>
2021-01-05 14:29:43 +0700

    menambahkan frame dan panelFrame di file GUI.java

--- GUI.java
+++ GUI.java
@@ -1,9 +1,48 @@
-public class GUI
-{
+public class GUI extends javax.swing.JFrame
+{  
+    private javax.swing.JPanel panelFrame;
 
-    public GUI() 
-    {
-        
+    public GUI() {
+        initComponents();
+        setExtendedState(JFrame.MAXIMIZED_HORIZ);
+        setVisible(true);
+        setResizable(false);
     }
     
+    private void initComponents() {
+
+        panelFrame = new javax.swing.JPanel();
+        panelUtama = new javax.swing.JPanel();
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Aplikasi goPERTA");
+
+        panelFrame.setBackground(new java.awt.Color(196, 69, 105));
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
+    }
 }
