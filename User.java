1955b79c72a4f8b8b84b7a63565f54188768fd34
Siti Balqis Humaira <66017795+balqishumaira@users.noreply.github.com>
2021-01-05 15:40:59 +0700

    membuat method constructor, override method tambah dan kurangi

--- User.java
+++ User.java
@@ -1,3 +1,48 @@
+/**
+ * Program yang berisi class User
+ *
+ * @author Siti Balqis Humaira
+ */
 public class User implements TambahKurang {
 
+    private String email;
+    private String password;
+    private String nama;
+    private String alamat;
+    private String jenisKendaraan;
+    private String noHp;
+    private int saldo;
+  
+  /**
+	 * Method constructor User
+	 * @param email,password
+	 */
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.nama = "";
+        this.alamat = "";
+        this.jenisKendaraan = "";
+        this.noHp = "";
+        this.saldo = 0;
+    }
+  
+  /**
+	 * Method untuk menambahkan jumlah saldo User
+	 * @param saldo
+	 */
+	@Override
+	public void tambah(int saldo) {
+		this.saldo += saldo;
+	}
+
+	/**
+	 * Method untuk mengurangi jumlah saldo User
+	 * @param saldo
+	 */
+	@Override
+	public void kurangi(int saldo) {
+		this.saldo -= saldo;
+	}
+  
 }
