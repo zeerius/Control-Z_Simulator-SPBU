720af31d34adf43d639113f8f7c5b4ceafafaf05
IntanMalahayati <72296329+IntanMalahayati@users.noreply.github.com>
2021-01-08 19:04:29 +0700

    Menambah dokumentasi pada Premium.java

--- Premium.java
+++ Premium.java
@@ -1,18 +1,39 @@
+/**
+ * Class Premium mewakili SPBU untuk transaksi premium
+ *
+ * @author (Control-Z)
+ * @version (08/01/2021)
+ */
 public class Premium extends SPBU
 {
     private BahanBakar tangkiPremium;
 
+
+    /**
+     * Method constructor untuk class Premium.
+     * Harga premium perliter adalah Rp6500,-
+     */
     public Premium()
     {
         tangkiPremium = new BahanBakar(6500, "Premium", 50, 1000);
     }
-
+    
+    /**
+     * Method getter untuk tangki. 
+     * @return tangkiPremium
+     */    
     @Override
     public BahanBakar getTangki()
     {
         return this.tangkiPremium;
     }
-
+    
+    /**
+     * Method untuk mengisi minyak user.
+     * @param user pengguna jasa SPBU
+     * @param liter jumlah liter bahan bakar yang akan diisi
+     * @param biaya biaya transaksi
+     */    
     @Override
     public void isiMinyak(User user, int liter, int biaya)
     {
@@ -20,13 +41,23 @@ public class Premium extends SPBU
         user.kurangi(biaya);
         pendapatan += biaya;
     }
-
+    
+    /**
+    * Method untuk mengecek isi tangki.
+    * @param liter jumlah liter yang akan diisi
+    * @return      true jika isi tangki cukup untuk melakukan transaksi, false jika tidak
+    */
     @Override
     public boolean cekIsiTangki(int liter)
     {
         return tangkiPremium.getIsiTangki() >= liter;
     }
-
+    
+    /**
+    * Method untuk mengecek saldo user.
+    * @param saldo saldo user
+    * @return      true jika saldo user cukup untuk melakukan transaksi, false jika tidak
+    */
     @Override
     public boolean cekSaldoUser(int saldo)
     {
