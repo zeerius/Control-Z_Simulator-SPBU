4e6017161d2d14fcfa413999936b55e22a486ba4
IntanMalahayati <72296329+IntanMalahayati@users.noreply.github.com>
2021-01-08 18:50:15 +0700

    Menambah dokumentasi pada SPBU.java

--- SPBU.java
+++ SPBU.java
@@ -1,23 +1,56 @@
+/**
+ * Class SPBU merupakan class abstract.
+ *
+ * @author (Control-Z)
+ * @version (08/01/2021)
+ */
 public abstract class SPBU
 {
     public static int pendapatan = 0;
 
-    // constructor
+    /**
+     * Method constructor untuk class SPBU.
+     */
     public SPBU()
     {
 
     }
 
-    // abstract method
+    /**
+     * Method abstrac untuk mengisi minyak.
+     * 
+     * @param user pengguna jasa SPBU
+     * @param liter jumlah liter bahan bakar yang akan diisi
+     * @param biaya biaya transaksi
+     */
     public abstract void isiMinyak(User user, int liter, int biaya);
 
+    /**
+     * Method abstract getter untuk tangki.
+     */
     public abstract BahanBakar getTangki();
 
+    /**
+     * Method abstract untuk mengecek isi tangki.
+     * 
+     * @param liter jumlah liter bahan bakar yang akan diisi
+     * @return boolean
+     */
     public abstract boolean cekIsiTangki(int liter);
 
+    /**
+     * Method abstract untuk mengecek saldo user.
+     * 
+     * @param saldo saldo user
+     * @return boolean
+     */
     public abstract boolean cekSaldoUser(int saldo);
 
-    // keterangan
+    /**
+     * Method untuk memberitahukan pendapatan SPBU.
+     * 
+     * @return pendapatan SPBU
+     */
     public String toString()
     {
         return "Pendapatan sekarang adalah + " + pendapatan;
