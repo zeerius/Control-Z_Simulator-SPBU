228aa3aa6ae40ade100c984548e6e20b00e89c32
IntanMalahayati <72296329+IntanMalahayati@users.noreply.github.com>
2021-01-08 18:42:20 +0700

    Menambah dokumentasi pada TambahKurang.java

--- TambahKurang.java
+++ TambahKurang.java
@@ -1,5 +1,20 @@
+/**
+ * Interface untuk menambah dan mengurangi integer
+ *
+ * @author (Control-Z)
+ * @version (08/01/2021)
+ */
 public interface TambahKurang
 {
+    /**
+     * Method untuk menambahkan nilai.
+     * @param nilai
+     */
     void tambah(int nilai);
+    
+    /**
+     * Method untuk mengurangi nilai.
+     * @param nilai
+     */
     void kurangi(int nilai);
 }
